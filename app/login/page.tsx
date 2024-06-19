bdba6925477cb1419ccc4c1e6d1ea7bd31aca00e
gksrbanssla <gksrbanssla@naver.com>
2024-06-19 23:26:42 +0900

    initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import FormButton from "@/components/button";
+import Input from "@/components/input";
+import SocialLogin from "@/components/social-login";
+import { redirect } from "next/navigation"
+import { useFormStatus, useFormState } from 'react-dom';
+import { logIn } from "./actions";
+import { PASSWORD_MIN_LENGTH } from '../lib/constants';
+
+export default function LogIn() {
+    const [state, dispatch] = useFormState(logIn, null);
+    // useFormStatus Hook은 <form>태그의 자식 엘레먼트들에만 사용 가능함.
+    return (
+        <div className="flex flex-col gap-10 py-8 px-6">
+        <div className="flex flex-col gap-2 *:font-medium">
+            <h1 className="text-2xl">안녕하세요!</h1>
+            <h2 className="text-xl">Log in with email and password.</h2>
+        </div>
+        <form action={dispatch} className="flex flex-col gap-3">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
+        />
+        <FormButton text="Log in" />
+      </form>
+        <SocialLogin />
+        </div>
+    );
+}
